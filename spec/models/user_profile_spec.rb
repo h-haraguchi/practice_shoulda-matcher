de3957df0ba04ba46da69114daa662f808961d0c
h-haraguchi <account.cgtdnpck@me.com>
2018-12-22 17:11:48 +0900

    change require to rails_helper

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,4 +1,7 @@
-require 'rspec'
+require 'rails_helper'
 
 describe UserProfile do
+
+  it { should allow_value('http://foo.com').for(:website_url) }
+  it { should allow_value('http://bar.com').for(:website_url) }
 end
