c4eb3dad8725affd3f4dc69bf633cb5b0c2cd46a
h-haraguchi <account.cgtdnpck@me.com>
2018-12-22 20:19:11 +0900

    add some tests

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -4,8 +4,18 @@
 class UserProfile
   include ActiveModel::Model
   attr_accessor :website_url
+  attr_accessor :email
+  attr_accessor :absent_attribute
+  attr_accessor :terms_of_service
+  attr_accessor :some_value
+  attr_accessor :some_text
+  attr_accessor :zip_code
 
   validates :website_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp
-
-  has_secure_password
+  validates_absence_of :absent_attribute
+  validates_acceptance_of :terms_of_service
+  validates_confirmation_of :email
+  validates :some_value, inclusion: [1, 2, 3]
+  validates :some_text, length: { minimum: 10, maximum: 40, too_short: 'aaaa', too_long: 'bbbb' }
+  validates :zip_code, length: { is: 7 }
 end
--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -14,10 +14,42 @@ describe UserProfile do
     should_not allow_values(*urls).for(:website_url)
   end
 
-  # with is_expected.to
-  it { is_expected.to validate_presence_of(:website_url) }
+  context 'validate_presence_of' do
+    it { is_expected.to validate_presence_of(:website_url) }
+  end
 
-  context 'secure_password' do
-    it { is_expected.to have_secure_password }
+  context 'validate_absence_of' do
+    it { is_expected.to validate_absence_of(:absent_attribute) }
+  end
+
+  context 'validate_acceptance_of' do
+    it { is_expected.to validate_acceptance_of(:terms_of_service) }
+  end
+
+  context 'validate_confirmation_of' do
+    it { is_expected.to validate_confirmation_of(:email) }
+  end
+
+  context 'validate_inclusion_of' do
+    it do
+      is_expected.to validate_inclusion_of(:some_value).in_array([1, 2, 3])
+      is_expected.to validate_inclusion_of(:some_value).in_array([1, 2])
+      is_expected.to validate_inclusion_of(:some_value).in_array([1, 3])
+      is_expected.to validate_inclusion_of(:some_value).in_array([2, 3])
+      is_expected.to validate_inclusion_of(:some_value).in_array([1])
+      is_expected.to validate_inclusion_of(:some_value).in_array([2])
+      is_expected.to validate_inclusion_of(:some_value).in_array([3])
+      is_expected.not_to validate_inclusion_of(:some_value).in_array([0])
+      is_expected.not_to validate_inclusion_of(:some_value).in_array([1, 2, 3, 4])
+    end
+  end
+
+  context 'validate_length_of' do
+    it 'short and long' do is_expected.to validate_length_of(:some_text).is_at_least(10).is_at_most(40).with_short_message('aaaa').with_long_message('bbbb') end
+    it 'just' do is_expected.to validate_length_of(:zip_code).is_equal_to(7) end
+    it '限界値分析もやってる' do
+      # 10文字までだけど11文字で検証したらちゃんとバリデーションエラーになる
+      # is_expected.to validate_length_of(:some_text).is_at_least(11)
+    end
   end
 end
